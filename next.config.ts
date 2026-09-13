e00676861d99e609ee985b132b846a424e080630
Yohan <yohanlahiru86@gmail.com>
2026-09-13 23:22:03 +0530

    fix: basepath updated

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // Enables static export
-  basePath: "/senior-citizen-villa",
+  basePath: "/seniorcitizenvilla",
   images: {
     unoptimized: true, // Disables Next.js image optimization service for static hosting
     remotePatterns: [
