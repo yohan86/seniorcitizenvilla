9509068a9960799828cca74e2bb104b0a74002ca
Yohan <yohanlahiru86@gmail.com>
2026-09-14 20:04:02 +0530

    fix: update folder path for images

--- utils/prefix.js
+++ utils/prefix.js
@@ -6,5 +6,5 @@ const isProd = process.env.NODE_ENV === 'production';
  * Appends the GitHub Pages repository subfolder path if in production.
  * Usage: src={`${prefix}/images/hero.jpg`}
  */
-//export const prefix = isProd ? '/seniorcitizenvilla' : '';
-export const prefix = '';
+export const prefix = isProd ? '/seniorcitizenvilla' : '';
+//export const prefix = '';
